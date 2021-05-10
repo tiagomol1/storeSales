6faf94b10274476c04c838a4c2ff78a5ad1c4a14
Tiago Murilo <tiago.murilo08@gmail.com>
2021-05-10 11:25:44 -0300

    Primeiro commit do projeto

---
+++ backend/src/main/java/com/devStoreSales/storeSales/StoreSalesApplication.java
@@ -0,0 +1,13 @@
+package com.devStoreSales.storeSales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreSalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreSalesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devStoreSales/storeSales/StoreSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devStoreSales.storeSales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
